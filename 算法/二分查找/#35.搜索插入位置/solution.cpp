67190df95c6549f68f48af649a6e2bbf5d0fe7b7
Seven-wild <771093908@qq.com>
2020-10-20 07:52:18 +0800

    Create solution.cpp

---
+++ 算法/二分查找/#35.搜索插入位置/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+        
+    }
+};
