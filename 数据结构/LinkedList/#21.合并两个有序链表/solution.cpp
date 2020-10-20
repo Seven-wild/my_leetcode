81d2e125936dfe39be680cc31d3f64d0aa829d86
Seven-wild <771093908@qq.com>
2020-10-20 10:12:49 +0800

    Create solution.cpp

---
+++ 数据结构/LinkedList/#21.合并两个有序链表/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *l = new ListNode(-1);
+        ListNode *p = l;
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if (l1) {
+            p->next = l1;
+        } else {
+            p->next = l2;
+        }
+        return l->next;
+    }
+};
