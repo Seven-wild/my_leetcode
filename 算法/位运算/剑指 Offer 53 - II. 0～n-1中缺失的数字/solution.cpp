5c0941a0b699c88f7e02c7feed8986559f9464e2
Seven0821 <771093908@qq.com>
2020-10-23 15:57:28 +0800

    Create solution.cpp

---
+++ 算法/位运算/剑指 Offer 53 - II. 0～n-1中缺失的数字/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int  ans = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        return ans ^ nums.size();
+    }
+};
