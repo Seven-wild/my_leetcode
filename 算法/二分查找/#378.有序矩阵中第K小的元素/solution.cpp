8fa09304268556723800508803db0c14e7059d02
Seven0821 <771093908@qq.com>
2020-10-23 21:22:57 +0800

    Update solution.cpp

--- 算法/二分查找/#378.有序矩阵中第K小的元素/solution.cpp
+++ 算法/二分查找/#378.有序矩阵中第K小的元素/solution.cpp
@@ -17,10 +17,10 @@ public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
         int left = matrix[0][0], right = matrix[n-1][n-1];
-        while (left < right) {
+        while (left <= right) {
             int mid = left + ((right - left) >> 1);
             if (check(matrix, mid, k, n)) {
-                right = mid;
+                right = mid - 1 ;
             } else {
                 left = mid + 1;
             }
