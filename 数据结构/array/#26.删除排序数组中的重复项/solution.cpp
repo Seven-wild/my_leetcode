4d85351d9faad95eaaf05abd231b936895cd9cda
Seven0821 <771093908@qq.com>
2020-10-30 23:37:04 +0800

    Update solution.cpp

--- 数据结构/array/#26.删除排序数组中的重复项/solution.cpp
+++ 数据结构/array/#26.删除排序数组中的重复项/solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.empty()){
-            return 0;
+        if (nums.empty()) return 0;
+        int newl = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[newl]) {
+                nums[++newl] = nums[i];
+            }
         }
-        int new_length = 1;
-        int pre = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            if (pre != nums[i]) {
-                pre = nums[i];
-                nums[new_length++] = nums[i];
-            } 
-        }
-        return new_length;
+        return newl + 1;
     }
 };
