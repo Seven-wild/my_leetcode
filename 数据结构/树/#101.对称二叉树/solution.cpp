5469b7274eccb1038ab30ab33b702f5b70d6e067
Seven0821 <771093908@qq.com>
2020-11-20 03:41:17 +0800

    Update solution.cpp

--- 数据结构/树/#101.对称二叉树/solution.cpp
+++ 数据结构/树/#101.对称二叉树/solution.cpp
@@ -7,6 +7,31 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/*迭代
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        queue<TreeNode *> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty()) {
+            TreeNode *leftnode = q.front(); q.pop();
+            TreeNode *rightnode = q.front(); q.pop();
+            if (!leftnode && !rightnode) continue;
+            if (!leftnode && rightnode || leftnode && !rightnode || leftnode->val != rightnode->val) {
+                return false;
+            }
+            q.push(leftnode->left);
+            q.push(rightnode->right);
+            q.push(leftnode->right);
+            q.push(rightnode->left);
+        }
+        return true;
+    }
+};
+*/
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
