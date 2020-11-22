d1f8c978568ef099d7ae29da3ab9169fc89837af
Seven0821 <771093908@qq.com>
2020-11-23 01:15:12 +0800

    Update solution.cpp

--- 算法/DFS/#967.连续差相同的数字/solution.cpp
+++ 算法/DFS/#967.连续差相同的数字/solution.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> ans;
-    void func(int now, int left, int k) {
+    void dfs(int now, int left, int k) {
         if (left == 0) {
             ans.push_back(now);
             return;
         }
         if (now % 10 + k < 10) {
-            func(now * 10 + now % 10 + k, left - 1, k);
+            dfs(now * 10 + now % 10 + k, left - 1, k);
         }
         if (k != 0 && now % 10 - k >= 0) {
-            func(now * 10 + now % 10 - k, left - 1, k);
+            dfs(now * 10 + now % 10 - k, left - 1, k);
         }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         for (int i = 1; i < 10; i++) {
-            func(i, n - 1, k);
+            dfs(i, n - 1, k);
         }
         return ans;
     }
