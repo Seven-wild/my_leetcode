af69c4df23c9414a700f43b35851b664b2d246d4
Seven0821 <771093908@qq.com>
2020-11-24 04:17:33 +0800

    Update solution.cpp

--- 算法/二分查找/#33.搜索旋转排序数组/solution.cpp
+++ 算法/二分查找/#33.搜索旋转排序数组/solution.cpp
@@ -6,14 +6,14 @@ public:
             int mid = (left + right) >> 1;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[0] <= nums[mid]) {
-                if (target >= nums[0] && target < nums[mid]) {
+            } else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                if (target > nums[mid] && target <= nums[nums.size() - 1]) {
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
