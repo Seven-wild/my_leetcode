5e4f8f1e758b8489a677df4240debf6f3e2557eb
Seven0821 <771093908@qq.com>
2020-11-29 23:53:42 +0800

    Create solution.cpp

---
+++ 算法/动态规划/#91.解码方法/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        if (n == 0 || s[0] == '0') return 0;
+        if (n == 1) return 1;
+        vector<int> dp(n);
+        dp[0] = 1;
+        if (s[1] == '0') {
+            if (s[0] > '2') return 0;
+            dp[1] = 1;
+        } else {
+            if (s[0] == '1') {
+                dp[1] = 2;
+            } else if (s[0] == '2' && s[1] <= '6') {
+                dp[1] = 2;
+            } else {
+                dp[1] = 1;
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (s[i] == '0') {
+                if (s[i - 1] == '0' || s[i - 1] > '2') return 0;
+                dp[i] = dp[i - 2];
+                continue;
+            }
+            if (s[i - 1] == '1') {
+                dp[i] = dp[i - 1] + dp[i - 2];
+                continue;
+            }
+            if (s[i - 1] == '2' && s[i] <= '6') {
+                dp[i] = dp[i - 1] + dp[i - 2];
+                continue;
+            } 
+            dp[i] = dp[i - 1];
+        }
+
+        return dp[n - 1];
+       
+    }
+};
