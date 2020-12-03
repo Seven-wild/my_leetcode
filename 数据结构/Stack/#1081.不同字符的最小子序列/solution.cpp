6ee181b2374d1da552c1652531004bf6edccb0eb
Seven0821 <771093908@qq.com>
2020-12-03 14:15:05 +0800

    Create solution.cpp

---
+++ 数据结构/Stack/#1081.不同字符的最小子序列/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<char> remain(26, -1);
+        for (auto ch : s) {
+            remain[ch - 'a']++;
+        }
+        int n = 0;
+        for (auto cnt : remain) {
+            if (cnt > -1) {
+                n++;
+            }
+        }
+        vector<char> stack(n);
+        vector<int> flag(26, 0);
+        int top = -1;
+        for (auto ch : s) {
+            if (flag[ch - 'a']) {
+                remain[ch - 'a']--;
+                continue;
+            }
+            while (top >= 0 && ch < stack[top] && remain[stack[top] - 'a'] > 0) {
+                flag[stack[top] - 'a'] = 0;
+                remain[stack[top] - 'a']--;
+                top--;
+            }
+            stack[++top] = ch;
+            flag[ch - 'a'] = 1;
+        }
+        string ans;
+        for (auto ch : stack) {
+            ans += ch;
+        }
+        return ans;
+    }
+};
