548b7c0e4cef54a2ec6c564a4567ca37d84480b1
Seven0821 <771093908@qq.com>
2020-12-19 21:32:54 +0800

    Update solution.cpp

--- 算法/二分查找/#4.寻找两个正序数组的中位数/solution.cpp
+++ 算法/二分查找/#4.寻找两个正序数组的中位数/solution.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        //选取较短的数组二分搜索
         if (nums1.size() > nums2.size()) {
             return findMedianSortedArrays(nums2, nums1);
         }
-        int m = nums1.size();
-        int n = nums2.size();
-        int lefttotal = (m + n + 1) / 2;
-        int l = 0, r = m;
-        while (l < r) {
-            int i = (l + r + 1) >> 1;
-            int j = lefttotal - i;
+        int m = nums1.size(), n = nums2.size();
+        //中位数左边元素总个数 （假设中位数总被分到左边）
+        int total_left = (m + n + 1) / 2;
+        //搜索区间[0, m]
+        int left = 0, right = m;
+        while (left < right) {
+            // 前半部分包括 nums1[0 .. i-1] 和 nums2[0 .. j-1]
+            // 后半部分包括 nums1[i .. m-1] 和 nums2[j .. n-1]
+            // nums1[i-1] < nums2[j] && nums2[m-1] < nums1[i]
+            int i = (left + right + 1) >> 1;
+            int j = total_left - i;
             if (nums1[i - 1] > nums2[j]) {
-                r = i - 1;
+                right = i - 1;
             } else {
-                l = i;
+                left = i;
             }
         }
-        int i = l;
-        int j = lefttotal - i;
-
-        int num1left = (i == 0 ? INT_MIN : nums1[i - 1]);
-        int num2left = (j == 0 ? INT_MIN : nums2[j - 1]);
-        int num1right = (i == m ? INT_MAX : nums1[i]);
-        int num2right = (j == n ? INT_MAX : nums2[j]);
-        return (m + n) % 2 == 1 ?  max(num1left, num2left) : (double) (max(num1left, num2left) + min(num1right, num2right)) / 2.0;
+        int i = left, j = total_left - i;
+        //注意特殊情况的处理
+        int nums1_left = (i == 0 ? INT_MIN : nums1[i - 1]);
+        int nums1_right = (i == m ? INT_MAX : nums1[i]);
+        int nums2_left = (j == 0 ? INT_MIN : nums2[j - 1]);
+        int nums2_right = (j == n ? INT_MAX : nums2[j]);
+        // m+n为奇数和偶数需判断
+        return (m + n) % 2 == 1 ? max(nums1_left, nums2_left) : (double)(max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2.0;
     }
 };
