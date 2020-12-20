d9103d0074bb2f8f42999599454e881fa1af7c64
Seven0821 <771093908@qq.com>
2020-12-20 17:24:10 +0800

    Update solution.cpp

--- 算法/动态规划/#5.最长回文字串/solution.cpp
+++ 算法/动态规划/#5.最长回文字串/solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        vector<vector<int> > dp(n, vector<int>(n));
+        int n = s.size();
         string ans;
-        for (int len = 0; len < n; len++) {
-            for (int i = 0; i + len < n; i++) {
-                int j = i + len;
-                if (len == 0) {
+        //定义状态 dp[i][j] == 0 表示s[i..j]不是回文字串
+        vector<vector<int>> dp(n, vector<int>(n));
+        //l为当前判断的子串长度
+        for (int l = 0; l < n; l++) {
+            //i为当前子串起始位置
+            for (int i = 0; i + l < n; i++) {
+                //j为当前子串结束位置
+                int j = i + l;
+                //状态转移
+                if (l == 0) {
                     dp[i][j] = 1;
-                } else if (len == 1) {
-                    dp[i][j] = (s[i] == s[j]);
+                } else if (l == 1) {
+                    dp[i][j] = s[i] == s[j];
                 } else {
                     dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
                 }
-                if (dp[i][j] && j - i + 1 > ans.length()) {
-                    ans = s.substr(i, len + 1);
+                //更新答案
+                if (dp[i][j] && j - i + 1 > ans.size()) {
+                    ans = s.substr(i, l + 1);
                 }
             }
         }
         return ans;
-
     }
 };
