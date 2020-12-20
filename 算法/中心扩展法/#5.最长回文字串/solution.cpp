716d59fcb2c681b7ddfcee00981a9b906bc99828
Seven0821 <771093908@qq.com>
2020-12-20 17:24:46 +0800

    Update solution.cpp

--- 算法/中心扩展法/#5.最长回文字串/solution.cpp
+++ 算法/中心扩展法/#5.最长回文字串/solution.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        if (n < 2) return s;
-        int begin;
-        int maxlen = 1;
-        for (int i = 0; i < n - 1; i++) {
-            int len1 = juage(s, i, i);
-            int len2 = juage(s, i, i + 1);
-            int cur_max = max(len1, len2);
-            if (cur_max > maxlen) {
-                begin = i - (cur_max - 1) / 2;
-                maxlen = cur_max;
-            }
-        }
-        return s.substr(begin, maxlen  );
-    }
-    int juage(const string &s, int left, int right) {
-        while (left >=0 && right < s.length() && s[left] == s[right]) {
+    pair<int, int> expand(const string &s, int left, int right) {
+        int n = s.size();
+        //不断向两边扩展
+        while (left >= 0 && right < n &&  s[left] == s[right]) {
             left--;
             right++;
         }
-        return right - left - 1; 
+        return {left + 1, right - 1};
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            //以s[i]为中心
+            auto [left1, right1] = expand(s, i, i);
+            //以s[i]和s[i+1]为中心
+            auto [left2, right2] = expand(s, i, i + 1);
+            //更新答案
+            if (right1 - left1 > end - start) {
+                start = left1;
+                end = right1;
+            }
+            if (right2 - left2 > end - start) {
+                start = left2;
+                end = right2;
+            }
+        }
+        string ans = s.substr(start, end - start + 1);
+        return ans;
     }
 };
